e5bd90cb6773f7809fe533ce79b20ffd47dd6b84
Nguyen Thanh Nam <namnguyen160504@gmail.com>
2025-07-14 02:31:38 +0700

    fix: cors error

--- server.js
+++ server.js
@@ -10,8 +10,22 @@ const authRoute = require('./app/routes/authRoute'); // Importing the auth route
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 app.use(cookieParser());
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use('/auth', authRoute);
 
